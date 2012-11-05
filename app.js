e6515f6e67d68ecbb2409ffa2b8d1d7b25012685
Ulrich Sossou <sorich87@gmail.com>
2012-11-05 18:09:38 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , app = express()
+  , beautify_html = require('./beautify-html');
+
+app.use(express.bodyParser());
+
+app.post('/', function (req, res) {
+  var beautiful
+    , source = req.body.source;
+
+  beautiful = beautify_html(source, {
+      'indent_char': "\t"
+    , 'indent_size': 1
+    , 'max_char': 0
+    , 'unformatted': [
+        'a', 'abbr', 'b', 'cite', 'code', 'del', 'dfn', 'command', 'em', 'font'
+      , 'i', 'img', 'input', 'ins', 'kbd', 'mark', 'meter', 'q', 'small'
+      , 'source', 'span', 'strong', 'sub', 'sup', 'time', 'var']
+  });
+
+  res.set('Content-Type', 'text/plain');
+  res.send(beautiful);
+});
+
+app.all('*', function (req, res) {
+  res.send('');
+});
+
+app.listen(process.env.VCAP_APP_PORT || 3000);
